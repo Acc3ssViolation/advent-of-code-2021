5cdc59dfc17823713113120159d21fd2cef5a810
Wouter Brookhuis <wrdbrookhuis@gmail.com>
2021-12-07 23:06:49 +0100

    Add basic Pi Pico project

---
+++ Pico/hello_world.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+
+#define MAX_COMMAND_ENTRIES   (40)
+
+#define LED_PIN (25)
+
+typedef void (*CommandHandler_t)(const char *commandString, size_t commandStringLength);
+
+
+typedef struct
+{
+  const char* command;
+  CommandHandler_t handler;
+} CommandEntry_t;
+
+
+static CommandEntry_t m_commandEntries[MAX_COMMAND_ENTRIES];
+static uint8_t m_nrOfCommandEntries;
+
+static bool AddCommand(const CommandEntry_t* command);
+
+static void LedOnCommand(const char *commandString, size_t commandStringLength);
+static void LedOffCommand(const char *commandString, size_t commandStringLength);
+
+int main()
+{
+  stdio_init_all();
+
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  const CommandEntry_t ledOnEntry = {
+      .command = "led on",
+      .handler = LedOnCommand,
+  };
+
+  const CommandEntry_t ledOffEntry = {
+      .command = "led off",
+      .handler = LedOffCommand,
+  };
+
+  AddCommand(&ledOnEntry);
+  AddCommand(&ledOffEntry);
+
+  char readBuffer[50];
+  memset(readBuffer, 0, sizeof(readBuffer));
+  uint8_t index = 0;
+
+  while(true)
+  {
+    int chr = getchar_timeout_us(100000ul);
+    if (chr != PICO_ERROR_TIMEOUT)
+    {
+      if (chr == 0x7F || chr == 0x08)
+      {
+        if (index > 0)
+        {
+          index--;
+
+          // Move back, print space, move back again
+          putchar(0x08);
+          putchar(0x20);
+          putchar(0x08);
+        }
+      }
+      else if (chr == 0x0D)
+      {
+        if (index > 0)
+        {
+          printf("\n");
+          readBuffer[index] = 0x00;
+
+          CommandHandler_t handler = NULL;
+
+          for (uint8_t i = 0; i < m_nrOfCommandEntries; i++)
+          {
+            if (strncmp(m_commandEntries[i].command, readBuffer, sizeof(readBuffer)) == 0)
+            {
+              handler = m_commandEntries[i].handler;
+              break;
+            }
+          }
+
+          if (handler != NULL)
+          {
+            handler(readBuffer, index + 1);
+          }
+          else
+          {
+            printf("Unknown command %s", readBuffer);
+          }
+
+          index = 0;
+        }
+        printf("\n>");
+      }
+      else if (index < sizeof(readBuffer) - 1)
+      {
+        readBuffer[index] = chr;
+        putchar(chr);
+
+        index++;
+      }
+    }
+
+    //gpio_xor_mask(1 << LED_PIN);
+  }
+  return 0;
+}
+
+static bool AddCommand(const CommandEntry_t* command)
+{
+  if (m_nrOfCommandEntries < MAX_COMMAND_ENTRIES)
+  {
+    m_commandEntries[m_nrOfCommandEntries++] = *command;
+    return true;
+  }
+  return false;
+}
+
+static void LedOnCommand(const char *commandString, size_t commandStringLength)
+{
+  gpio_put(LED_PIN, 1);
+  printf("LED on");
+}
+
+static void LedOffCommand(const char *commandString, size_t commandStringLength)
+{
+  gpio_put(LED_PIN, 0);
+  printf("LED off");
+}
