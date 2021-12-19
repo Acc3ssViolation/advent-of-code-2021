f145ec95e04529f40029029adf2eba173413624e
Acc3ssViolation <wrdbrookhuis@gmail.com>
2021-12-19 15:15:46 +0100

    Day 08 wip

--- Adv.NET/Assignments/Day08.cs
+++ Adv.NET/Assignments/Day08.cs
@@ -52,22 +52,94 @@ namespace Adv.NET.Assignments
 
         protected override void RunPart(IReadOnlyList<LogEntry> loadedData, Part part)
         {
-            if (part == Part.Two)
-                return;
+            if (part == Part.One)
+            {
+                var stuffs = 0;
 
-            var stuffs = 0;
+                foreach (var entry in loadedData)
+                {
+                    foreach (var output in entry.Output)
+                    {
+                        var numSegments = CountActiveSegments(output);
+                        if (numSegments == 3 || numSegments == 2 || numSegments == 4 || numSegments == 7)
+                            stuffs++;
+                    }
+                }
+
+                Console.WriteLine($"Digits 1, 4, 7 and 8 appeared {stuffs} times");
+                return;
+            }
 
             foreach (var entry in loadedData)
             {
-                foreach (var output in entry.Output)
+                var potentialPositions = new Segment[7];
+
+                foreach (var output in entry.Patterns)
                 {
                     var numSegments = CountActiveSegments(output);
-                    if (numSegments == 3 || numSegments == 2 || numSegments == 4 || numSegments == 7)
-                        stuffs++;
+                    if (numSegments == 2)
+                    {
+                        // This is a 1
+                        if (potentialPositions[2] == Segment.None)
+                            potentialPositions[2] = output;
+                        else
+                            potentialPositions[2] &= output;
+                        if (potentialPositions[5] == Segment.None)
+                            potentialPositions[5] = output;
+                        else
+                            potentialPositions[5] &= output;
+                    }
+                    else if (numSegments == 3)
+                    {
+                        // This is a 7
+                        if (potentialPositions[0] == Segment.None)
+                            potentialPositions[0] = output;
+                        else
+                            potentialPositions[0] &= output;
+                        if (potentialPositions[2] == Segment.None)
+                            potentialPositions[2] = output;
+                        else
+                            potentialPositions[2] &= output;
+                        if (potentialPositions[5] == Segment.None)
+                            potentialPositions[5] = output;
+                        else
+                            potentialPositions[5] &= output;
+                    }
+                    else if (numSegments == 4)
+                    {
+                        // This is a 4
+                        if (potentialPositions[1] == Segment.None)
+                            potentialPositions[1] = output;
+                        else
+                            potentialPositions[1] &= output;
+                        if (potentialPositions[2] == Segment.None)
+                            potentialPositions[2] = output;
+                        else
+                            potentialPositions[2] &= output;
+                        if (potentialPositions[3] == Segment.None)
+                            potentialPositions[3] = output;
+                        else
+                            potentialPositions[3] &= output;
+                        if (potentialPositions[5] == Segment.None)
+                            potentialPositions[5] = output;
+                        else
+                            potentialPositions[5] &= output;
+                    }
+                    else if (numSegments == 7)
+                    {
+                        // This is an 8
+                        for (int i = 0; i < potentialPositions.Length; i++)
+                        {
+                            if (potentialPositions[i] == Segment.None)
+                                potentialPositions[i] = output;
+                            else
+                                potentialPositions[i] &= output;
+                        }
+                    }
                 }
-            }
 
-            Console.WriteLine($"Digits 1, 4, 7 and 8 appeared {stuffs} times");
+                Console.WriteLine("");
+            }
         }
 
         private static Segment ParseSegments(string input)
